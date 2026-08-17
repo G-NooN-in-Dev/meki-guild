d7e1b9fc4ef1be1fc8b4e4576dda573fb10c0039
G-NooN Jo <jjjchoon@nate.com>
2026-08-17 18:23:13 +0900

    fix/style: Collapsible - base-ui 기준 animation 적용

--- packages/ui/src/collapsible.tsx
+++ packages/ui/src/collapsible.tsx
@@ -2,6 +2,8 @@
 
 import { Collapsible as CollapsiblePrimitive } from '@base-ui/react/collapsible'
 
+import { cn } from './lib/utils'
+
 function Collapsible({ ...props }: CollapsiblePrimitive.Root.Props) {
 	return <CollapsiblePrimitive.Root data-slot="collapsible" {...props} />
 }
@@ -10,8 +12,17 @@ function CollapsibleTrigger({ ...props }: CollapsiblePrimitive.Trigger.Props) {
 	return <CollapsiblePrimitive.Trigger data-slot="collapsible-trigger" {...props} />
 }
 
-function CollapsibleContent({ ...props }: CollapsiblePrimitive.Panel.Props) {
-	return <CollapsiblePrimitive.Panel data-slot="collapsible-content" {...props} />
+/** 높이는 Panel에서 transition. Base UI가 --collapsible-panel-height와 data-starting/ending-style을 여기에 둡니다 */
+function CollapsibleContent({ className, children, ...props }: CollapsiblePrimitive.Panel.Props) {
+	return (
+		<CollapsiblePrimitive.Panel
+			data-slot="collapsible-content"
+			className="h-(--collapsible-panel-height) overflow-hidden transition-[height] duration-200 ease-out data-ending-style:h-0 data-starting-style:h-0"
+			{...props}
+		>
+			<div className={cn(className)}>{children}</div>
+		</CollapsiblePrimitive.Panel>
+	)
 }
 
 export { Collapsible, CollapsibleContent, CollapsibleTrigger }
